84c21323768002bfd6d9e6bde581e094eaeb5460
Nathan Gromb <nathan.gromb@epfl.ch>
2025-11-30 14:25:01 +0100

    Fix config

--- src/owtp/config.py
+++ src/owtp/config.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from typing import Union
 
 def load_yaml_config(file_path: Union[str, Path] = 'config/config.yaml') -> dict:
-    with open(file_path, 'r') as f:
+    if isinstance(file_path, str):
+        file_path = Path(file_path)
+
+    with open(file_path.resolve(), 'r') as f:
         config = yaml.safe_load(f)
     return config
