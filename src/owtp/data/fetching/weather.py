d07bc140799d194b101a4868d129aa3c548bf9eb
Nathan Gromb <nathan.gromb@epfl.ch>
2025-11-30 15:58:55 +0100

    Start weather fetch and update config

---
+++ src/owtp/data/fetching/weather.py
@@ -0,0 +1,62 @@
+import owtp.config
+import requests
+from requests import Response
+import regex
+from pathlib import Path
+from tqdm import tqdm
+
+class HourlyWeatherDataFetcher:
+    def __init__(self):
+        self.config = owtp.config.load_yaml_config()
+
+        self.base_dir = Path(self.config['paths']['raw_data']) / "weather/hourly"
+
+        self.catalog_base_url = "https://thredds-su.ipsl.fr/thredds/catalog/aeris_thredds/actrisfr_data/665029c8-82b8-4754-9ff4-d558e640b0ba"
+        self.catalog_url_suffix = "catalog.html"
+
+        self.file_base_url = "https://thredds-su.ipsl.fr/thredds/fileServer/aeris_thredds/actrisfr_data/665029c8-82b8-4754-9ff4-d558e640b0ba"
+
+#        self.year_range = range(1845, 2024)
+        self.year_range = range(2000, 2002)
+
+
+    def fetch_weather_data(self):
+        all_data = []
+        for year in tqdm(self.year_range, desc="Fetching yearly weather data"):
+            year_url = f"{self.catalog_base_url}/{year}/{self.catalog_url_suffix}"
+            response = requests.get(year_url)
+            stations = self.parse_year_available_stations(response)
+            if response.status_code != 200:
+                print(f"Failed to fetch data for year {year}: {response.status_code}. Error message: {response.text}")
+                continue
+
+            for station_file in tqdm(stations, desc=f"Fetching station data for year {year}", leave=False):
+                station_file = self.base_dir / str(year) / station_file
+                if station_file.exists():
+                    continue
+                station_file.parent.mkdir(parents=True, exist_ok=True)
+
+                station_url = f"{self.file_base_url}/{year}/{station_file}"
+                station_response = requests.get(station_url)
+                if station_response.status_code != 200:
+                    print(f"Failed to fetch station file {station_file} for year {year}: {station_response.status_code}. Error message: {station_response.text}")
+                    continue
+
+                with open(station_file, "w+b") as f:
+                    f.write(station_response.content)
+            
+        return all_data
+    
+    def parse_year_available_stations(self, reponse: Response) -> list[str]:
+        PATTERN = r"<a\s+[^>]*href=[\"'][^\"']*/([^/]+\.nc)[\"'][^>]*>"
+
+        return regex.findall(PATTERN, reponse.text, regex.IGNORECASE)
+
+if __name__ == "__main__":
+    config = owtp.config.load_yaml_config()
+
+    fetcher = HourlyWeatherDataFetcher()
+
+    data = fetcher.fetch_weather_data()
+
+    print(f"Fetched data for {len(data)} stations.")
