4cce85372015e87e2c11ce75ac4ccaa626f16ccc
Nathan Gromb <nathan.gromb@epfl.ch>
2026-01-02 14:36:43 +0100

    Make windows return bounds and take start and end

--- src/owtp/others/rolling.py
+++ src/owtp/others/rolling.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from owtp.config import load_yaml_config
 
-def get_windows(df: pd.DataFrame):
+def get_windows(start, end):
     """Generate sliding windows of the DataFrame based on the configured parameters."""
     config = load_yaml_config()
 
@@ -10,9 +10,6 @@ def get_windows(df: pd.DataFrame):
     step_size = config['rolling_calibrations']['step_size']
     gap_size = config['rolling_calibrations']['gap_size']
     eval_size = config['rolling_calibrations']['eval_size']
-
-    start = df.index.min()
-    end = df.index.max()
 
     windows = []
     current_start = start
@@ -22,10 +19,12 @@ def get_windows(df: pd.DataFrame):
         eval_start = train_end + pd.Timedelta(seconds=1) + pd.Timedelta(days=gap_size)
         eval_end = eval_start + pd.Timedelta(days=eval_size) - pd.Timedelta(seconds=1)
 
-        train_window = df.loc[train_start:train_end]
-        eval_window = df.loc[eval_start:eval_end]
-
-        windows.append((train_window, eval_window))
+        windows.append({
+            "train_window_start": train_start,
+            "train_window_end": train_end,
+            "eval_window_start": eval_start,
+            "eval_window_end": eval_end,
+        })
 
         current_start += pd.Timedelta(days=step_size)
 
